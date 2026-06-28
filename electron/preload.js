db304d5fe615ab8a61d339843857b2b437abd859
かるみーる <225147902+calmeel@users.noreply.github.com>
2026-06-29 01:20:48 +0900

    exeを改良

--- electron/preload.js
+++ electron/preload.js
@@ -1,5 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  onOsuMapInfo: (callback) => ipcRenderer.on('osu-map-info', (_, data) => callback(data)),
+  onOsuMapInfo:  (cb) => ipcRenderer.on('osu-map-info',    (_, data) => cb(data)),
+  onTimingInfo:  (cb) => ipcRenderer.on('osu-timing-info', (_, data) => cb(data)),
+  getCurrentMapset: (knownFolder) => ipcRenderer.invoke('osu-get-current-mapset', knownFolder),
+  minimize:      ()   => ipcRenderer.send('win-minimize'),
+  maximize:      ()   => ipcRenderer.send('win-maximize'),
+  close:         ()   => ipcRenderer.send('win-close'),
+  openDocs:      ()   => ipcRenderer.send('win-open-docs'),
+  onMaximize:    (cb) => ipcRenderer.on('win-maximized',   cb),
+  onUnmaximize:  (cb) => ipcRenderer.on('win-unmaximized', cb),
 });
